7a2b007dca594fc6d08fe5709e6f6e7560808170
Pollentinha <144285242+pollentinha@users.noreply.github.com>
2025-05-21 21:45:41 -0300

    Add files via upload

---
+++ scripts/objRefTable.js
@@ -0,0 +1,49 @@
+const C3 = self.C3;
+self.C3_GetObjectRefTable = function () {
+	return [
+		C3.Plugins.Sprite,
+		C3.Behaviors.Platform,
+		C3.Behaviors.bound,
+		C3.Behaviors.solid,
+		C3.Plugins.Text,
+		C3.Plugins.System.Cnds.EveryTick,
+		C3.Plugins.System.Acts.Scroll,
+		C3.Plugins.Sprite.Exps.X,
+		C3.Plugins.Sprite.Exps.Y,
+		C3.Plugins.Text.Acts.SetText,
+		C3.Plugins.Sprite.Cnds.OnCollision,
+		C3.Plugins.System.Acts.RestartLayout,
+		C3.Plugins.System.Acts.SetVar,
+		C3.Plugins.Sprite.Acts.Destroy,
+		C3.Plugins.System.Acts.AddVar,
+		C3.Plugins.System.Cnds.Compare,
+		C3.Plugins.Text.Acts.SetVisible
+	];
+};
+self.C3_JsPropNameTable = [
+	{Plataforma: 0},
+	{RestritoAoLayout: 0},
+	{Sprite: 0},
+	{Sólido: 0},
+	{Sprite2: 0},
+	{Sprite3: 0},
+	{Sprite4: 0},
+	{Sprite5: 0},
+	{Sprite6: 0},
+	{Sprite7: 0},
+	{Texto: 0},
+	{Texto2: 0},
+	{pontos: 0}
+];
+
+self.InstanceType = {
+	Sprite: class extends self.ISpriteInstance {},
+	Sprite2: class extends self.ISpriteInstance {},
+	Sprite3: class extends self.ISpriteInstance {},
+	Sprite4: class extends self.ISpriteInstance {},
+	Sprite5: class extends self.ISpriteInstance {},
+	Sprite6: class extends self.ISpriteInstance {},
+	Sprite7: class extends self.ISpriteInstance {},
+	Texto: class extends self.ITextInstance {},
+	Texto2: class extends self.ITextInstance {}
+}
